6306fe410aea5d0a53ed796e5e73bf2876d0516c
Orion-Technologies <77119730+Orion-Technologies@users.noreply.github.com>
2024-02-19 19:38:15 -0600

    Update CarritoContext.jsx
    eliminación de comentarios

--- src/context/CarritoContext.jsx
+++ src/context/CarritoContext.jsx
@@ -1,36 +1,26 @@
-//1-  Importar el hook useState y createContext
 import {createContext, useState} from "react";
 
-//2- creamos el nuevo contexto, valor inicial un objeto con las siguientes propiedades:
 export const CarritoContext = createContext({
     carrito: [],
     total: 0,
     cantidadTotal: 0
 })
 
-//3- Creamos un componente llamado "CarritoProvider", se puede encontrar ene l material com : "Proveedor de Contextos".
 export const CarritoProvider = ({children}) => {
-    //Usamos useState para generar algunos estados para almacenar el carrito, el total y la cantidadTotal.
 
     const [carrito, setCarrito] = useState([]);
     const [total, setTotal] = useState(0);
     const [cantidadTotal, setCantidadTotal] = useState(0);
 
-    //Metemos unos console.log de forma momentanea para ver que todo se este actualizando correctamente
     console.log(carrito);
     console.log("Monto total de la compra: ", total);
     console.log("Cantidad de items: ", cantidadTotal);
 
-    //4- agregamos algunos metodos al proveedor de contexto para manipular el carrito de compras.
     const agregarAlCarrito = (item, cantidad) => {
         const productoExistente = carrito.find(prod => prod.item.id === item.id);
 
         if (!productoExistente) {
-        /*
-        * La sintaxis
-        * prev => [...prev, {item, cantidad}]
-        * se utiliza para crear un nuevo array a partir del estado anterior del carrito (prev) y agregar un nuevo objeto, que representa el nuevo producto
-        * */
+     
             setCarrito(prev => [...prev, {item, cantidad}]);
             setCantidadTotal(prev => prev + cantidad);
             setTotal(prev => prev + (item.precio * cantidad));
@@ -47,9 +37,8 @@ export const CarritoProvider = ({children}) => {
             setTotal(prev => prev + (item.precio * cantidad))
         }
     }
-    //Funcion para eliminar del carrito.
+    
     const eliminarProducto = (id) => {
-        // Me guardo una referencia del producto que vamos a borrar:
 
         const productoEliminado = carrito.find(prod => prod.item.id === id);
         const carritoActualizado = carrito.filter(prod => prod.item.id !== id);
@@ -59,7 +48,6 @@ export const CarritoProvider = ({children}) => {
         setTotal(prev => prev - (productoEliminado.item.precio * productoEliminado.cantidad))
     }
 
-    //Función para vaciar el carrito de compras:
     const vaciarCarrito = () => {
         setCarrito([]);
         setTotal(0);
@@ -68,5 +56,4 @@ export const CarritoProvider = ({children}) => {
     return (
         <CarritoContext.Provider value = {{carrito, total, cantidadTotal, agregarAlCarrito, eliminarProducto, vaciarCarrito}}>{children}</CarritoContext.Provider>
     )
-    // Le tenemos que agregar el children, que es una propiedad especial que utilizamos
 }
